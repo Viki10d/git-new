e476ec876f52fd8f4c5da8fe6ca66ce808272038
Viki10d <vikasdnaik2003@gmail.com>
2025-07-09 22:44:52 +0530

    version 11

--- change.js
+++ change.js
@@ -1,8 +1,5 @@
 console.log('chnage.js');
-<<<<<<< HEAD
-console.log('version 6');
-=======
 console.log('version 9');
->>>>>>> 6b8092a5500d10a3cf6b4f5dfb509c7d3d4f7e69
+
 
 
--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 console.log('config.js');
-<<<<<<< HEAD
-console.log('version 7');
-=======
-console.log('version 9');
->>>>>>> 6b8092a5500d10a3cf6b4f5dfb509c7d3d4f7e69
+console.log('version 11');
+
